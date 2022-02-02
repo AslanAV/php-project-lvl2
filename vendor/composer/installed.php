ba494f3f24bc91523c2a5cb5b4cea8d36c797b6f
Aslan Autlev <autlevaslan@gmail.com>
2022-02-02 12:01:41 +0300

    fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '042b5d3dd49cec826903b0caa0ea5b59848ad806',
+        'name' => 'hexlet/code',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'docopt/docopt' => array(
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../docopt/docopt',
+            'aliases' => array(),
+            'reference' => 'bf3683a16e09fa1665e493eb4d5a29469e132a4f',
+            'dev_requirement' => false,
+        ),
+        'hexlet/code' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '042b5d3dd49cec826903b0caa0ea5b59848ad806',
+            'dev_requirement' => false,
+        ),
+        'roave/security-advisories' => array(
+            'pretty_version' => 'dev-latest',
+            'version' => 'dev-latest',
+            'type' => 'metapackage',
+            'install_path' => NULL,
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '31d9d9e2977ae7d796d82271be09e46f4bdf41b3',
+            'dev_requirement' => true,
+        ),
+    ),
+);
