d1f6ff35dacb936e6e1389a56605127a03be5655
Aslan Autlev <autlevaslan@gmail.com>
2022-02-15 12:17:12 +0300

    add fix Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -13,10 +13,10 @@ function gendiff(string $firstFile, string $secondFile, string $format = 'stylis
     $keys = array_unique(array_merge(array_keys($firstFixtures), array_keys($secondFixtures)));
     asort($keys);
     $result = array_reduce($keys, function ($acc, $key) use ($firstFixtures, $secondFixtures){
-        if (array_key_exists($key, $firstFixtures) && gettype($firstFixtures[$key]) === "boolean") {
+        if (array_key_exists($key, $firstFixtures) && is_bool($firstFixtures[$key])) {
             $firstFixtures[$key] = ($firstFixtures[$key] === true) ? "true" : "false";
         }
-        if (array_key_exists($key, $secondFixtures) && gettype($secondFixtures[$key]) === "boolean") {
+        if (array_key_exists($key, $secondFixtures) && is_bool($secondFixtures[$key])) {
             $secondFixtures[$key] = ($secondFixtures[$key] === true) ? "true" : "false";
         }
         if (array_key_exists($key, $firstFixtures) && array_key_exists($key, $secondFixtures)) {
@@ -28,7 +28,7 @@ function gendiff(string $firstFile, string $secondFile, string $format = 'stylis
             }
         } elseif (array_key_exists($key, $firstFixtures)) {
             $acc = "{$acc} -  {$key}: {$firstFixtures[$key]}\n";
-        } else {
+        } elseif (array_key_exists($key, $secondFixtures)) {
             $acc = "{$acc} +  {$key}: {$secondFixtures[$key]}\n";
         }
         return $acc;
