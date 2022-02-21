847f1b4e988260385ea9fd7bee59715b21f305a6
Aslan.Autlev <autlevaslan@gmail.com>
2022-02-21 12:03:53 +0300

    fix lint

--- src/Differ.php
+++ src/Differ.php
@@ -2,6 +2,9 @@
 
 namespace Hexlet\Code\Differ;
 
+/**
+ * @throws \Exception
+ */
 function gendiff(string $firstFile, string $secondFile, string $format = 'stylish'): string
 {
     $firstFile = fullPathToFile($firstFile);
@@ -14,28 +17,35 @@ function gendiff(string $firstFile, string $secondFile, string $format = 'stylis
     $secondFixtures = jsonDecode($secondFileContent);
     $secondFixtures = normalizeBoolean($secondFixtures);
 
-    $result = diffFixtures($firstFixtures, $secondFixtures);
-    return $result;
+    return diffFixtures($firstFixtures, $secondFixtures);
 }
 
 function fullPathToFile(string $file): string
 {
     if (substr($file, 1) !== '/') {
-        $firstFile = __DIR__ . '/../' . $file;
+        $file = __DIR__ . '/../' . $file;
     }
     return $file;
 }
 
-function jsonDecode(string $fileContent): array
+/**
+ * @param mixed $fileContent
+ * @return mixed
+ * @throws \Exception
+ */
+function jsonDecode($fileContent)
 {
-    if ($fileContent === false) {
-        throw new \Exception("Can't read file: " . $fileContent);
+    if ($fileContent == false) {
+        throw new \Exception("Can't read file");
     }
-    $fixtures = json_decode($fileContent, true);
-    return $fixtures;
+    return json_decode($fileContent, true);
 }
 
-function normalizeBoolean(array $fixtures): array
+/**
+ * @param mixed $fixtures
+ * @return mixed
+ */
+function normalizeBoolean($fixtures)
 {
     foreach ($fixtures as $key => $item) {
         if (is_bool($item)) {
@@ -45,10 +55,16 @@ function normalizeBoolean(array $fixtures): array
     return $fixtures;
 }
 
-function diffFixtures(array $firstFixtures, array $secondFixtures): string
+/**
+ * @param mixed $firstFixtures
+ * @param mixed $secondFixtures
+ * @return string
+ */
+function diffFixtures($firstFixtures, $secondFixtures): string
 {
     $keys = array_unique(array_merge(array_keys($firstFixtures), array_keys($secondFixtures)));
     asort($keys);
+    print_r($firstFixtures);
     $result = array_reduce($keys, function ($acc, $key) use ($firstFixtures, $secondFixtures) {
         if (array_key_exists($key, $firstFixtures) && array_key_exists($key, $secondFixtures)) {
             if ($firstFixtures[$key] === $secondFixtures[$key]) {
