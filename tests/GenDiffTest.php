4741004cbb7034f0c78a2d370756e09a172e04d5
Aslan.Autlev <autlevaslan@gmail.com>
2022-03-03 16:32:11 +0300

    add test YAML

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -8,23 +8,23 @@ use function Hexlet\Code\Differ\gendiff;
 
 class GenDiffTest extends TestCase
 {
-//    public function testDiffTwoJSON(): void
-//    {
-//        $firstFile1 = "/tests/fixtures/file1.json";
-//        $secondFile2 = "/tests/fixtures/file2.json";
-//        $resultTwoJSON = gendiff($firstFile1, $secondFile2);
-//        $expectedTwoJSON = file_get_contents(__DIR__ . "/fixtures/expectedTwoJSON.txt");
-//        $this->assertEquals($expectedTwoJSON, $resultTwoJSON);
-//    }
+    public function testDiffTwoJSON(): void
+    {
+        $firstFile1 = "/tests/fixtures/file1.json";
+        $secondFile2 = "/tests/fixtures/file2.json";
+        $resultTwoJSON = gendiff($firstFile1, $secondFile2);
+        $expectedTwoJSON = file_get_contents(__DIR__ . "/fixtures/expectedTwoJSON.txt");
+        $this->assertEquals($expectedTwoJSON, $resultTwoJSON);
+    }
 
-//    public function testDiffTwoYAML(): void
-//    {
-//        $firstFile1 = "/tests/fixtures/filepath1.yml";
-//        $secondFile2 = "/tests/fixtures/filepath2.yml";
-//        $resultTwoYML = gendiff($firstFile1, $secondFile2);
-//        $expectedTwoYML = file_get_contents(__DIR__ . "/fixtures/expectedTwoYML.txt");
-//        $this->assertEquals($expectedTwoYML, $resultTwoYML);
-//    }
+    public function testDiffTwoYAML(): void
+    {
+        $firstFile1 = "/tests/fixtures/filepath1.yml";
+        $secondFile2 = "/tests/fixtures/filepath2.yml";
+        $resultTwoYML = gendiff($firstFile1, $secondFile2);
+        $expectedTwoYML = file_get_contents(__DIR__ . "/fixtures/expectedTwoYML.txt");
+        $this->assertEquals($expectedTwoYML, $resultTwoYML);
+    }
 
     public function testDiffTwoJSONRecursive(): void
     {
@@ -32,6 +32,15 @@ class GenDiffTest extends TestCase
         $secondFile2 = "/tests/fixtures/filepath2.json";
         $resultTwoJSON = gendiff($firstFile1, $secondFile2);
         $expectedTwoJSON = file_get_contents(__DIR__ . "/fixtures/expectedTwoJSONRecursive.txt");
+        $this->assertEquals($expectedTwoJSON, $resultTwoJSON);
+    }
+
+    public function testDiffTwoYAMLRecursive(): void
+    {
+        $firstFile1 = "/tests/fixtures/fileRecursive1.yaml";
+        $secondFile2 = "/tests/fixtures/fileRecursive2.yaml";
+        $resultTwoJSON = gendiff($firstFile1, $secondFile2);
+        $expectedTwoJSON = file_get_contents(__DIR__ . "/fixtures/expectedTwoYAMLRecursive.txt");
         $this->assertEquals($expectedTwoJSON, $resultTwoJSON);
     }
 }
